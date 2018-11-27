528ae1f63c67873cc701302ecc3a582f23a84302
Rachel <Racheltyq19@gmail.com>
2018-11-27 19:19:50 +0800

    Update decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+string inverted_seq(string a)
+{
+  string opstr="";
+  return opstr;
+}
+int bin_to_dec(){string a}
+{
+  int dec=0;
+  return dec;
+}
+void decode_ldrstr(string a){}
+void decode_operate(string a){}
+void decode_mul(string a){}
+void decode_mrs(string a){}
+void decode_swi(string a){}
+void decode_branche(string a){}
+void classification(string line){}
+void openfiles(){}
 int main()
 {
+  return 0;
 }
